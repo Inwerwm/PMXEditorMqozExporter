0671ca9f3429f15edd0738d6467d552ad8ed327f
Inwerwm <saclainvervm@gmail.com>
2020-02-24 14:46:19 +0900

    プロジェクトを作成

---
+++ MqozExporter/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PEPlugin;
+using PEPlugin.Pmx;
+
+namespace MqozExporter
+{
+    public class MqozExporter : IPEExportPlugin
+    {
+        public string Ext
+        {
+            get
+            {
+                return ".mqoz";
+            }
+        }
+        public string Caption
+        {
+            get
+            {
+                return "Zipped Metasequoia Object";
+            }
+        }
+        public void Export(IPXPmx pmx, string path, IPERunArgs args)
+        {
+
+        }
+    }
+}
